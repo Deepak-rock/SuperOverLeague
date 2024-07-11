ed3bf86586bee775c8bae00af12cfef47d5e4fde
Deepak <deepakind777@gmail.com>
2024-07-11 13:55:08 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const headingChild = 'Super Over Leaque'
+const element = (
+  <div className='bg-container'>
+    <h1 className='heading'>{headingChild}</h1>
+    <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' />
+    <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png' />
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
